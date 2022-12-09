db92cc8e09eb777c5020015ed7e1222bd79b7bd9
itSMeBigMan <97398194+itSMeBigMan@users.noreply.github.com>
2022-12-09 16:15:15 +0800

    Add files via upload

---
+++ q2 ans.js
@@ -0,0 +1,28 @@
+/*
+    Task 1: 
+    - Declare an array that are going to be used to store patient's name.
+
+    Task 2: 
+    - Add code to add patient's name into the array declared in task 1.
+
+    Task 3: 
+    - Implement listPatient() function to print all patient's name stored in the array
+*/
+
+
+// Task 1: const Array = [];
+
+function addPatient(patientName){
+    // Task 2: const Array = ["John", "Mary", "Mark"];
+}
+
+// Task 3: Add code here
+
+function Array() { return []; }
+
+addPatient("John");
+addPatient("Mary");
+addPatient("Mark");
+alert(Array("John", "Mary", "Mark"));
+
+listPatient(); // This should list ["John", "Mary", "Mark"]
